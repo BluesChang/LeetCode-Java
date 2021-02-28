3b5f83af83a3661055c83caa21b63ceb78a9a5a8
changyu <realchangyu@gmail.com>
2021-02-28 21:51:46 +0800

    add 剑指 Offer 04. 二维数组中的查找

--- 剑指offer/04. 二维数组中的查找/Solution.java
+++ 剑指offer/04. 二维数组中的查找/Solution.java
@@ -5,6 +5,7 @@
 //        首先选取数组中右上角的数字。如果该数字。如果该数字等于要查找的数字，则查找过程结束；如果该数字大于要查找的数字，则剔除
 //        这个数字所在的列；如果该数字小于要查找的数字，则剔除这个数字所在的行。也就是说，如果要查找的数字不在数组的右上角，则每
 //        一次都在数组的查找范围中剔除一行或者一列，这样每一步都可以缩小查找的范围，直到找到要查找的数字，或者查找范围为空。
+
 class Solution {
     public boolean findNumberIn2DArray(int[][] matrix, int target) {
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
