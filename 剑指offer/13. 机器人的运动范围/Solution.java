5d5587e3ed0bf5bd367c18a43fae94bdab56d47d
changyu <realchangyu@gmail.com>
2021-03-04 23:16:35 +0800

    add 剑指 Offer 13. 机器人的运动范围

---
+++ 剑指offer/13. 机器人的运动范围/Solution.java
@@ -0,0 +1,34 @@
+//题目：
+//        地上有一个m行和n列的方格。一个机器人从坐标0,0的格子开始移动，每一次只能向左，右，上，下四个方向移动一格，
+//        但是不能进入行坐标和列坐标的数位之和大于k的格子。 例如，当k为18时，机器人能够进入方格（35,37），因为3+5+3+7 = 18。
+//        但是，它不能进入方格（35,38），因为3+5+3+8 = 19。请问该机器人能够达到多少个格子？
+
+class Solution {
+    int m, n, k;
+    boolean[][] visited;
+
+    public int movingCount(int m, int n, int k) {
+        this.m = m;
+        this.n = n;
+        this.k = k;
+        this.visited = new boolean[m][n];
+        return dfs(0, 0);
+    }
+
+    private int dfs(int i, int j) {
+        if (i >= m || j >= n || getDigitSum(i) + getDigitSum(j) > k || visited[i][j]) {
+            return 0;
+        }
+        visited[i][j] = true;
+        return 1 + dfs(i + 1, j) + dfs(i, j + 1);
+    }
+
+    private int getDigitSum(int number) {
+        int sum = 0;
+        while (number > 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
+}
