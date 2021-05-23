af3f967941c084640a73d9ef4c3c42ac2fb04171
changyu <realchangyu@gmail.com>
2021-05-23 14:52:33 +0800

    add 剑指 Offer 18. 删除链表的节点

---
+++ 剑指offer/18. 删除链表的节点/Solution.java
@@ -0,0 +1,27 @@
+//题目：
+//在O(1)时间内删除链表节点。给定单向链表的头指针和一个节点指针，定义一个函数在O(1)时间内删除该节点。
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode deleteNode(ListNode head, int val) {
+        if (head.val == val) {
+            return head.next;
+        }
+        ListNode pre = head, cur = head.next;
+        while (cur != null && cur.val != val) {
+            pre = cur;
+            cur = cur.next;
+        }
+        if (cur != null) {
+            pre.next = cur.next;
+        }
+        return head;
+    }
+}
