acce4a628e93bd617198f3da236bd5e885fdcd0f
changyu <realchangyu@gmail.com>
2021-05-23 20:25:22 +0800

    add 剑指 Offer 24. 反转链表

---
+++ 剑指offer/24. 反转链表/Solution.java
@@ -0,0 +1,22 @@
+//题目：定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode newHead = null;
+        while (head != null) {
+            ListNode tmp = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = tmp;
+        }
+        return newHead;
+    }
+}
